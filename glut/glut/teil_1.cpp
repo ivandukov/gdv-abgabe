7194d349e62ee2e18167fb88ab14e7c3968def23
Sasa Bojanic <sasa.bojanic@stud.h-da.de>
2022-02-19 12:01:37 +0100

    added robot movement in four directions

--- glut/glut/teil_1.cpp
+++ glut/glut/teil_1.cpp
@@ -3,12 +3,19 @@
 #include "Wuerfel.h"
 using namespace std;
 
-GLfloat l_legrotation;
-GLfloat r_legrotation;
-int legstate = 0;
-GLfloat r_armrotation;
-GLfloat l_armrotation;
-int armstate = 0;
+bool ismovingforward = false;
+bool ismovingbackwards = false;
+bool ismovingleft = false;
+bool ismovingright = false;
+
+double translateforwardbackwards = 0.0;
+double translateleftright = 0.0;
+
+void move_robot()
+{
+    glTranslatef(0.0, 0.0, translateforwardbackwards); // tatsaechliche Bewegung d. Baggers (vorwaerts)
+    glTranslatef(translateleftright, 0.0, 0.0); // tatsaechliche Bewegung d. Baggers (seitlich)
+}
 
 void draw_cylinder(GLfloat radius, GLfloat height, GLubyte R, GLubyte G, GLubyte B)
 {
@@ -48,6 +55,110 @@ void draw_cylinder(GLfloat radius, GLfloat height, GLubyte R, GLubyte G, GLubyte
     glEnd();
 }
 
+void handleUserInput(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'w':
+    {
+        ismovingforward = true;
+        break;
+    }
+    case 's':
+    {
+        ismovingbackwards = true;
+        break;
+    }
+    case 'a':
+    {
+        ismovingleft = true;
+        break;
+    }
+    case 'd':
+    {
+        ismovingright = true;
+        break;
+    }
+    }
+}
+
+double handleRobotForwardMovement(double current_z)
+{
+    if (ismovingforward)
+    {
+        if (current_z <= -7.00)
+        {
+            current_z = -7.00;
+        }
+        else
+        {
+            current_z += 0.1;
+            ismovingforward = false;
+        }
+    }
+    return current_z;
+}
+
+double handleRobotBackwardsMovement(double current_z)
+{
+    if (ismovingbackwards)
+    {
+        if (current_z <= -7.00)
+        {
+            current_z = -7.00;
+        }
+        else
+        {
+            current_z -= 0.1;
+            ismovingbackwards = false;
+        }
+    }
+    return current_z;
+}
+
+double handleRobotLeftMovement(double current_x)
+{
+    if (ismovingleft)
+    {
+        if (current_x <= -7.00)
+        {
+            current_x = -7.00;
+        }
+        else
+        {
+            current_x += 0.1;
+            ismovingleft = false;
+        }
+    }
+    return current_x;
+}
+
+double handleRobotRightMovement(double current_x)
+{
+    if (ismovingright)
+    {
+        if (current_x <= -7.00)
+        {
+            current_x = -7.00;
+        }
+        else
+        {
+            current_x -= 0.1;
+            ismovingright = false;
+        }
+    }
+    return current_x;
+}
+
+void handleCamera()
+{
+    //Kameraposition, Blickwinkel und Up-Vector
+    //gluLookAt(0.75, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht schraeg
+    //gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht von vorne
+    gluLookAt(0.0, 0.5, -1.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht von hinten
+}
+
+
 void draw_robot()
 {
     glPushMatrix();
@@ -66,14 +177,14 @@ void draw_robot()
     glPushMatrix();
     glRotatef(90, 1., 0., 0.);
     glTranslatef(0.13, 0.14, -0.02);
-    Wuerfel(0.05, 0.2, 0.05); // Wuerfel(0.05, 0.1, 0.05); 
+    Wuerfel(0.05, 0.2, 0.05);
     glPopMatrix();
 
     // linker Arm
     glPushMatrix();
     glRotatef(90, 1., 0., 0.);
     glTranslatef(-0.13, 0.14, -0.02);
-    Wuerfel(0.05, 0.2, 0.05); //  Wuerfel(0.05, 0.1, 0.05);
+    Wuerfel(0.05, 0.2, 0.05);
     glPopMatrix();
 
     // linkes Rad
@@ -92,6 +203,11 @@ void draw_robot()
     glPopMatrix();
 }
 
+
+/**
+ * @brief erstellt die Flaeche, auf der sich der Roboter
+ *        bewegen soll
+ */
 void draw_floor()
 {
     glPushMatrix();
@@ -119,21 +235,20 @@ void Init()
 
 /**
  * @brief Code der in jedem Frame ausgefuehrt werden soll
- * Die Arm- und Beinrotation geschehen um die x-Achse
+ *        Die Arm- und Beinrotation geschehen um die x-Achse
  */
 void RenderScene()
 {
     glLoadIdentity(); // Aktuelle Model-/View-Transformations-Matrix zuruecksetzen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Kameraposition, Blickwinkel und Up-Vector
-    //gluLookAt(0.75, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht schraeg
-    //gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht von vorne
-    gluLookAt(0.0, 0.5, -1.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht von hinten
+    handleCamera();
 
     draw_floor();
+    glPushMatrix();
+    move_robot();
     draw_robot();
-
+    glPopMatrix();
 
     glutSwapBuffers();
 }
@@ -171,6 +286,11 @@ void Animate(int value)
     // Timer wieder registrieren - Animate wird so nach 10 msec mit value+=1 aufgerufen.
     int wait_msec = 10;
     glutTimerFunc(wait_msec, Animate, ++value);
+
+    translateforwardbackwards = handleRobotForwardMovement(translateforwardbackwards);
+    translateforwardbackwards = handleRobotBackwardsMovement(translateforwardbackwards);
+    translateleftright = handleRobotLeftMovement(translateleftright);
+    translateleftright = handleRobotRightMovement(translateleftright);
 }
 
 int main(int argc, char** argv)
@@ -183,6 +303,7 @@ int main(int argc, char** argv)
     glutReshapeFunc(Reshape);
     // TimerCallback registrieren; wird nach 10 msec aufgerufen mit Parameter 0  
     glutTimerFunc(10, Animate, 0);
+    glutKeyboardFunc(handleUserInput);
     Init();
     glutMainLoop();
     return 0;
