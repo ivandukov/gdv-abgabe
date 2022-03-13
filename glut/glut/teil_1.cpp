92b0e514c71714cc4b817ed890e4ee399f8bef1a
Sabo2k <63151233+Sabo2k@users.noreply.github.com>
2022-03-13 12:29:40 +0100

    Update teil_1.cpp

--- glut/glut/teil_1.cpp
+++ glut/glut/teil_1.cpp
@@ -2,8 +2,10 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <map>
 #include <fstream>
-#include <GL/freeglut.h> 
+#include <GL/freeglut.h>
+#include <GL/SOIL.h>
 using namespace std;
 #include "Wuerfel.h"
 #include "Objects.h"
@@ -26,31 +28,37 @@ float cubeZmin = 0.0;
 float cubeZmax = 0.0;
 
 /**
- * wird inkrementiert, sobald ein 
+ * Zeitlimit in ms
+ */
+int timeLeft = 60000;
+
+/**
+ * true, wenn player collected everything
+ * or time is up
+ */
+bool gameOver = false;
+
+/**
+ * wird inkrementiert, sobald ein
  * Cube gesammelt wurde, damit der naechste
  * gespawnt werden kann
  */
 int currentCubeIndex = 0;
 
 /**
- * Rotation des Cubes 
+ * Rotation des Cubes
  */
-GLfloat drehwinkel = 0.0; 
+GLfloat drehwinkel = 0.0;
 
 /**
- * facing direction des Roboters 
+ * facing direction des Roboters
  */
 float currentangle = 0.0;
 
 /**
- * aktueller Punktestand des Spielers 
+ * aktueller Punktestand des Spielers
  */
-int points = 0;  
-
-/**
- * Countdown des Spiels in ms 
- */
-int timeLeft = 60000;
+int points = 0;
 
 struct CubeCoordinate
 {
@@ -62,12 +70,12 @@ struct CubeCoordinate
  * alle Koordinaten der Cubes,
  * die der Roboter sammeln soll
  */
-vector<CubeCoordinate> cubecoordinates {};
+vector<CubeCoordinate> cubecoordinates{};
 
 /**
  * @name  dateilesen
- * @brief Hier werden alle Koordinaten aus coordinates.txt für die 
- *        sammelbaren Gegenstände gelesen und in cubecoordinates 
+ * @brief Hier werden alle Koordinaten aus coordinates.txt für die
+ *        sammelbaren Gegenstände gelesen und in cubecoordinates
  *        gespeichert,
  *        wird in main() aufgerufen
  */
@@ -75,7 +83,7 @@ void readFile()
 {
     ifstream inFile("coordinates.txt");
 
-    if(!inFile.is_open())
+    if (!inFile.is_open())
     {
         throw invalid_argument("coordinates.txt cannot be opened!\n");
     }
@@ -83,12 +91,12 @@ void readFile()
     string line {};
     while(getline(inFile, line))
     {
-        string XPOS {};
-        string ZPOS {};
+        string XPOS{};
+        string ZPOS{};
         istringstream iss(line);
         getline(iss, XPOS, ';');
         getline(iss, ZPOS);
-        CubeCoordinate* newCubeCoordinate = new CubeCoordinate; 
+        CubeCoordinate* newCubeCoordinate = new CubeCoordinate;
         newCubeCoordinate->xpos = stof(XPOS);
         newCubeCoordinate->zpos = stof(ZPOS);
         cubecoordinates.push_back(*newCubeCoordinate);
@@ -108,18 +116,19 @@ void handleCamera()
     //gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);  // Ansicht von vorne
     //gluLookAt(0.0, 0.7, -1.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht von hinten
     //gluLookAt(0.0, 5.0, -1.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Ansicht von oben
-    // gluLookAt(0.0, 0.7, 1.5, currentX, 0.0, currentZ, 0.0, 1.0, 0.0); // folgende Kamera
-    gluLookAt(currentX, 5.0, currentZ - 1.5, currentX, 0.0, currentZ, 0.0, 1.0, 0.0); // Ansicht von oben (folgend)
+    //gluLookAt(0.0, 0.7, 1.5, currentX, 0.0, currentZ, 0.0, 1.0, 0.0); // folgende Kamera
+    //gluLookAt(currentX, 5.0, currentZ - 1.5, currentX, 0.0, currentZ, 0.0, 1.0, 0.0); // Ansicht von oben (folgend)
+    gluLookAt(currentX, 4.0, currentZ - 1.9, currentX, 0.0, currentZ, 0.0, 1.0, 0.0); // Ansicht von oben (folgend)
 }
 
 /**
  * @name  keyPressedDown
- * @brief Hier reagiert das Programm auf gedrückte Tasten 
+ * @brief Hier reagiert das Programm auf gedrückte Tasten
  *        wird in main() aufgerufen
  */
 void keyPressedDown(unsigned char key, int x, int y)
 {
-    switch(key)
+    switch (key)
     {
         case 'w': // vorwaerts
         {
@@ -144,7 +153,7 @@ void keyPressedDown(unsigned char key, int x, int y)
         case 'e':
         {
             cout << "\nx = " << currentX << endl
-                 << "z = " << currentZ << "\n\n";
+                << "z = " << currentZ << "\n\n";
             break;
         }
         default:
@@ -199,37 +208,40 @@ void keyPressedUp(unsigned char key, int x, int y)
  */
 void robot_movement()
 {
-    if(movingUp)
+    if(gameOver == false)
     {
-        currentangle = 360.0;
-        currentZ += 0.02f;
-    }
-    else if(movingDown)
-    {
-        currentangle = 180.0;
-        currentZ -= 0.02f;
-    }
-    else if(movingLeft)
-    {
-        currentangle = 90.0;
-        currentX += 0.02f;
-    }
-    else if (movingRight)
-    {
-        currentangle = 270.0;
-        currentX -= 0.02f;
+        if (movingUp)
+        {
+            currentangle = 360.0;
+            currentZ += 0.02f;
+        }
+        else if (movingDown)
+        {
+            currentangle = 180.0;
+            currentZ -= 0.02f;
+        }
+        else if (movingLeft)
+        {
+            currentangle = 90.0;
+            currentX += 0.02f;
+        }
+        else if (movingRight)
+        {
+            currentangle = 270.0;
+            currentX -= 0.02f;
+        }
     }
 }
 
 /**
  * @name  displayText
- * @brief gibt den String text in RGB-Farben aus 
+ * @brief gibt den String text in RGB-Farben aus
  */
-void displayText(float x, float y, int r, int g, int b, string text) 
+void displayText(float x, float y, float z, int r, int g, int b, string text)
 {
     glColor3f(r, g, b);
-    glRasterPos2f(x, y);
-    for(int i = 0; i < text.size(); i++) 
+    glRasterPos3f(x, y, z);
+    for(int i = 0; i < text.size(); i++)
     {
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, text[i]);
     }
@@ -237,26 +249,30 @@ void displayText(float x, float y, int r, int g, int b, string text)
 
 /**
  * @name  spawnCube
- * @brief spawnt 1 drehenden cube aus cubecoordinates 
+ * @brief spawnt 1 drehenden cube aus cubecoordinates
  *        und setzt  "Hitbox"-Koordinaten,
- *        wird in RenderScene() aufgerufen 
+ *        wird in RenderScene() aufgerufen
  */
 void spawnCube(int index)
 {
-     CubeCoordinate cube = cubecoordinates[index];
+    if (index < cubecoordinates.size()) // out of bounds verhindern
+    {
+        CubeCoordinate cube = cubecoordinates[index];
 
-     // Detektionsgrenzen der Cubes definieren
-     cubeXmin = cube.xpos - 0.1f;
-     cubeXmax = cube.xpos + 0.1f;
-     cubeZmin = cube.zpos - 0.1f;
-     cubeZmax = cube.zpos + 0.1f;
-     
-     glPushMatrix();
+        // Detektionsgrenzen der Cubes definieren
+        cubeXmin = cube.xpos - 0.1f;
+        cubeXmax = cube.xpos + 0.1f;
+        cubeZmin = cube.zpos - 0.1f;
+        cubeZmax = cube.zpos + 0.1f;
+
+        glPushMatrix();
         glNormal3d(0, 0, 1);
         glTranslatef(cube.xpos, 0.0, cube.zpos); // Wuerfel positionieren
         glRotatef(drehwinkel, 0.0, 1.0, 0.0);   // Wuerfel in Drehung versetzen 
         Wuerfel_mit_Normalen(0.1, 0.1, 0.1); // Wuerfel erzeugen
-     glPopMatrix();
+        glPopMatrix();
+    }
+
 }
 
 /**
@@ -267,14 +283,14 @@ void spawnCube(int index)
 void handleLight()
 {
     glEnable(GL_LIGHTING); // enable lighting
-    glEnable(GL_LIGHT0);
-    GLfloat light_position[] = {currentX, 1.0, currentZ, 1.0};
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position); // Licht Nr. 0 rechts oben
-    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHT0);   // Es gibt ein Licht Nr. 0 
+
+    GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
     glEnable(GL_COLOR_MATERIAL);
     glClearDepth(1.0);
     glEnable(GL_NORMALIZE);
-    
 }
 
 /**
@@ -286,6 +302,7 @@ void Init()
 {
     glClearColor(0, 0, 0, 1);
     handleLight();
+    loadAllTextures();
     glEnable(GL_DEPTH_TEST); // z-Buffer
 }
 
@@ -301,7 +318,6 @@ void RenderScene()
 
     handleCamera(); // Kameraposition und Richtung
     draw_floor();   // Boden erzeugen
-
     glPushMatrix();
         robot_movement(); // Berechnung der Bewegung d. Roboters
         glTranslatef(currentX, 0.0, currentZ); // Tatsaechliche Bewegung
@@ -310,6 +326,17 @@ void RenderScene()
     glPopMatrix();
 
     spawnCube(currentCubeIndex); // spawnt Cube aus cubecoordinates
+
+    draw_rocket();
+   
+    displayText(currentX + 2.1, 0.0, currentZ + 2.1, 0, 1, 0, "time left: " + to_string(timeLeft / 1000));
+    displayText(currentX + 2.1, 0.0, currentZ + 1.9, 0, 1, 0, "points: " + to_string(points -1 ));
+    if(timeLeft <= 0 || currentCubeIndex >= cubecoordinates.size())
+    {
+        gameOver = true;
+        displayText(currentX + 1.5, 0.0, currentZ, 0, 1, 0, "GAME OVER!");
+        displayText(currentX + 1.5, 0.0, currentZ - 0.5 , 0, 1, 0, "YOU HAVE COLLECTED: " + to_string(points - 1) + " CUBES IN 1 MINUTE");
+    }
 
     glutSwapBuffers();
     glFlush(); // forces execution of OpenGL functions in finite time.
@@ -325,7 +352,6 @@ void Reshape(int width, int height)
     glMatrixMode(GL_PROJECTION); // Matrix fuer Transformation: Frustum -> viewport
     glLoadIdentity(); // Akt. Transformations-Matrix zuruecksetzen
     glViewport(0, 0, width, height);      // Viewport definieren
-    //glOrtho(-1., 1., -1., 1., 0., 10.); // orthogonale Kamera
     gluPerspective(45.0, 1, 0.1, 10.0);   // perspektivische Kamera
     glMatrixMode(GL_MODELVIEW); // Matrix fuer Modellierung/Viewing
 }
@@ -333,7 +359,7 @@ void Reshape(int width, int height)
 /**
  * @name  animateCube
  * @param uebernimmt Drehung der einzusammelnden Cubes
- *        und die "Hitbox", prueft also ob der Roboter 
+ *        und die "Hitbox", prueft also ob der Roboter
  *        einen Cube eingesammelt hat.
  *        wird in Animate() aufgerufen
  */
@@ -346,13 +372,16 @@ void animateCube()
         drehwinkel -= 360.0f;
     }
 
-    // Pruefen, ob der Cube vom Roboter eingesammelt wurde
-    if((currentX <= cubeXmax && currentX >= cubeXmin) &&
-       (currentZ <= cubeZmax && currentZ >= cubeZmin))
+    if (gameOver == false)
     {
-        cout << "gotcha!\n";
-        currentCubeIndex++;
-        points = currentCubeIndex;
+        // Pruefen, ob der Cube vom Roboter eingesammelt wurde
+        if ((currentX <= cubeXmax && currentX >= cubeXmin) &&
+            (currentZ <= cubeZmax && currentZ >= cubeZmin))
+        {
+            cout << "gotcha!\n";
+            currentCubeIndex++;
+            points = currentCubeIndex;
+        }
     }
 }
 
@@ -367,7 +396,11 @@ void Animate(int value)
 {
     glutPostRedisplay(); // RenderScene aufrufen
     int wait_msec = 10; // Timer wieder registrieren 
-    glutTimerFunc(wait_msec, Animate, ++value); // Animate wird nach 10 msec mit value+=1 aufgerufen
+    if (gameOver == false)
+    {
+        glutTimerFunc(wait_msec, Animate, timeLeft -= 10);
+    }
+    //glutTimerFunc(wait_msec, Animate, ++value); // Animate wird nach 10 msec mit value+=1 aufgerufen
     animateCube();
 }
 
