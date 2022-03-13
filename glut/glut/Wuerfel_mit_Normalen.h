c895fc6876014a30edcda7febe812b67351ae419
Sabo2k <63151233+Sabo2k@users.noreply.github.com>
2022-03-13 12:30:54 +0100

    Update Wuerfel_mit_Normalen.h

--- glut/glut/Wuerfel_mit_Normalen.h
+++ glut/glut/Wuerfel_mit_Normalen.h
@@ -1,2 +1,8 @@
 void Wuerfel_mit_Normalen(GLfloat fSeitenL);
 void Wuerfel_mit_Normalen(GLfloat fSeitenLX, GLfloat fSeitenLY, GLfloat fSeitenLZ);
+void draw_floor();
+void loadAllTextures();
+void Roboter_Kopf(GLfloat fSeitenLX, GLfloat fSeitenLY, GLfloat fSeitenLZ);
+void Roboter_Body(GLfloat fSeitenLX, GLfloat fSeitenLY, GLfloat fSeitenLZ);
+void Wuerfel_blau(GLfloat fSeitenLX, GLfloat fSeitenLY, GLfloat fSeitenLZ);
+void Wuerfel_schrott(GLfloat fSeitenLX, GLfloat fSeitenLY, GLfloat fSeitenLZ);
